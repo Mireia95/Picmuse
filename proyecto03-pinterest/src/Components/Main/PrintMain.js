04cb095ecdbbc387ba8f3a7b26faffeb03e70244
Mireia <mireia95.granzotto@gmail.com>
2024-07-08 18:29:43 +0200

    2024/07/08 clean code
    borrados comentarios inutiles

--- proyecto03-pinterest/src/Components/Main/PrintMain.js
+++ proyecto03-pinterest/src/Components/Main/PrintMain.js
@@ -10,36 +10,3 @@ export const printSectionPhoto = () => {
   sectionGallery.id = 'gallery';
   main.appendChild(sectionGallery);
 };
-
-/* export const printPhotos = (arrayPhotos) => {
-  const input = document.querySelector('#search');
-  input.value = '';
-  const sectionGallery = document.querySelector('#gallery');
-  for (const photo of arrayPhotos) {
-    const article = document.createElement('article');
-    const imgDiv = document.createElement('div');
-    const a = document.createElement('a');
-    a.href = photo.links.html;
-    const img = document.createElement('img');
-    img.src = photo.urls.small;
-
-    const creatorDivImg = document.createElement('div');
-    const creatorImg = document.createElement('img');
-    creatorImg.src = photo.user.profile_image.medium;
-
-    const creatorName = document.createElement('h3');
-    creatorName.innerText = photo.user.name;
-
-    article.classList.add('article-container');
-    creatorDivImg.classList.add('creatorDivImg');
-
-    a.appendChild(img);
-    creatorDivImg.appendChild(creatorImg);
-    imgDiv.appendChild(a);
-    imgDiv.appendChild(creatorDivImg);
-    article.appendChild(imgDiv);
-    article.appendChild(creatorName);
-
-    sectionGallery.appendChild(article);
-  }
-}; */
--- proyecto03-pinterest/src/Components/Main/PrintResults.js
+++ proyecto03-pinterest/src/Components/Main/PrintResults.js
@@ -9,21 +9,6 @@ import { printPhotos } from '../utils/PrintPhotos';
 //*variables globales
 let photosAPI = [];
 
-//!peticion API
-/* export const getUnsplashPeticion = async (state) => {
-  try {
-    const clientID = '4jlvdYuLsqNEEm_9Swt8i8YsjYiof2h9BhIUpHUVzTM';
-    const APIurl = `https://api.unsplash.com/search/photos?query='${state.word}'&page=${state.page}&per_page=20&client_id=${clientID}`;
-
-    const res = await fetch(APIurl);
-    const APIresults = await res.json();
-    photosAPI = APIresults.results;
-    return photosAPI;
-  } catch (error) {
-    console.log('Error API peticion' + error);
-  }
-}; */
-
 //*si la busqueda está sin resultados: imprime alert. Si no, imprime fotos
 export const printResults = async (state) => {
   state.page = 1;
